173ec0021f6dd39900b296836830f45d86a4b3d9
Yuva Punnam <venkatgowrish07@gmail.com>
2026-05-20 16:04:55 +0530

    Add files via upload

---
+++ 3.Circuits/1_combinational logic/basic gates/15_3_bit_population_count.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input [2:0] in,
+    output reg [1:0] out 
+    ); 
+  integer i;   
+  always @(*) begin
+      out = 2'b0;
+      for(i = 0; i < 3; i ++) begin
+          if(in[i])
+              out = out + 2'b1;
+     end
+  end
+endmodule
